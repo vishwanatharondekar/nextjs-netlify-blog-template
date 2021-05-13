3a134310ec223cfe6f3b69456723466804525fe3
Vishwanath Arondekar <vishwanath@dream11.com>
2021-05-13 20:16:09 +0530

    feat: tech talk page with dynamic data from template

--- src/pages/posts/[post].tsx
+++ src/pages/posts/[post].tsx
@@ -6,7 +6,7 @@ import matter from "gray-matter";
 import { fetchPostContent } from "../../lib/posts";
 import fs from "fs";
 import yaml from "js-yaml";
-import { parseISO } from 'date-fns';
+import { parseISO } from "date-fns";
 import PostLayout from "../../components/PostLayout";
 
 import InstagramEmbed from "react-instagram-embed";
@@ -25,8 +25,8 @@ export type Props = {
 
 const components = { InstagramEmbed, YouTube, TwitterTweetEmbed };
 const slugToPostContent = (postContents => {
-  let hash = {}
-  postContents.forEach(it => hash[it.slug] = it)
+  let hash = {};
+  postContents.forEach(it => (hash[it.slug] = it));
   return hash;
 })(fetchPostContent());
 
@@ -37,9 +37,9 @@ export default function Post({
   tags,
   author,
   description = "",
-  source,
+  source
 }: Props) {
-  const content = hydrate(source, { components })
+  const content = hydrate(source, { components });
   return (
     <PostLayout
       title={title}
@@ -51,22 +51,27 @@ export default function Post({
     >
       {content}
     </PostLayout>
-  )
+  );
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
   const paths = fetchPostContent().map(it => "/posts/" + it.slug);
   return {
     paths,
-    fallback: false,
+    fallback: false
   };
 };
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   const slug = params.post as string;
+
+  console.log(
+    "slugToPostContent[slug].fullPath : ",
+    slugToPostContent[slug].fullPath
+  );
   const source = fs.readFileSync(slugToPostContent[slug].fullPath, "utf8");
   const { content, data } = matter(source, {
-    engines: { yaml: (s) => yaml.load(s, { schema: yaml.JSON_SCHEMA }) as object }
+    engines: { yaml: s => yaml.load(s, { schema: yaml.JSON_SCHEMA }) as object }
   });
   const mdxSource = await renderToString(content, { components, scope: data });
   return {
@@ -78,7 +83,6 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
       tags: data.tags,
       author: data.author,
       source: mdxSource
-    },
+    }
   };
 };
-
--- src/pages/tech-talk/index.tsx
+++ src/pages/tech-talk/index.tsx
@@ -1,17 +1,44 @@
 import React from "react";
 import Head from "next/head";
+import { listPostContent, countPosts } from "../../lib/posts";
+import { config } from "process";
+import { listTags } from "../../lib/tags";
+
+import renderToString from "next-mdx-remote/render-to-string";
+import matter from "gray-matter";
+import fs from "fs";
+import yaml from "js-yaml";
+import path from "path";
+const components = {};
 
 const TechTalk = ({ htmlString, data }) => {
   console.log("htmlString: ", htmlString);
   console.log("data: ", data);
-  return <div style={{ margin: 10 }}>Tech Talk page here</div>;
+  return <div style={{ margin: 10 }}>{htmlString}</div>;
 };
 
-export const getStaticProps = async () => {
+export const getStaticProps = async ({ params }) => {
+  const source = fs.readFileSync(
+    path.join(process.cwd(), "content/tech-talk/tech-talk.mdx"),
+    "utf8"
+  );
+  const { content, data } = matter(source, {
+    engines: { yaml: s => yaml.load(s, { schema: yaml.JSON_SCHEMA }) as object }
+  });
+  console.log("source  :", source);
+  const mdxSource = await renderToString(content, { components, scope: data });
   return {
     props: {
-      htmlString: "html string here",
-      data: "data here"
+      data: {
+        title: data.title,
+        dateString: data.date,
+        slug: data.slug,
+        description: "",
+        tags: data.tags,
+        author: data.author,
+        source: mdxSource
+      },
+      htmlString: content
     }
   };
 };
